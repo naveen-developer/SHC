e22acf65c234e133077aba1309c7f81c778193f3
naveen-developer <you@example.com>
2024-08-25 21:25:57 +0530

    fourt commit

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,12 @@
 import { products } from "./products.js";
-console.log(products);
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
+//console.log(products);
 
-getYear();
-
-// overlay menu
-function openNav() {
-    document.getElementById("myNav").classList.toggle("menu_width");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
-}
-
-
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-
-// lightbox gallery
-$(document).on("click", '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
 
 
 
 let displayProducts = '';
 products.map((curElem) => {
-    displayProducts += `
+  displayProducts += `
     <div class="col-sm-6 col-md-4 col-lg-3">
           <div class="box">
             <a href="productDetail.html?id=${curElem.id}">
@@ -68,6 +38,6 @@ products.map((curElem) => {
 document.querySelector('.latestProduct').innerHTML = displayProducts;
 
 function imageDetail(event) {
-    event.preventDefault();
-    alert('image clicked');
+  event.preventDefault();
+  alert('image clicked');
 }
